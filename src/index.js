71a65c364d0b21f73efa355d9c185ae0f69c71e4
Roman Zhuk <rombat999@gmail.com>
2020-09-29 17:26:39 +0300

    feat: task completed in 20 min

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length == 0) return 0;
+array.sort( (a, b) => a - b )
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length == 0) return 0;
+array.sort( (a, b) => a - b );
+  return array[array.length-1]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length == 0) return 0;
+    let average = array.reduce((a, b) => a + b, 0)
+    return average/array.length;
+   
 }
